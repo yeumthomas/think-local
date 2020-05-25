c57b5d3f6c4533c05f259753dc681da5dd3e9fe7
Thomas Yeum <thomasyeum@Thomas-MacBook-Pro.local>
2020-05-25 01:25:45 -0500

    Updated router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
+import Home from './Home.vue'
+import Katy from './Katy.vue'
+import Contact from './Contact.vue'
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+//Set up Routes
+const routes = [
+  { path: '/', component: Home },
+  { path: '/katy', component: Katy },
+  { path: '/contact', component: Contact }
+];
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+});
+
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
 }).$mount('#app')

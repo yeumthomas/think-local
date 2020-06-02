162f0f70db6950268415ee9ca9054f06c3d5b268
Thomas Yeum <yeum.thomas@gmail.com>
2020-06-02 03:28:50 -0500

    Updated routers

--- src/main.js
+++ src/main.js
@@ -3,17 +3,29 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 import Home from './Home.vue'
-import Katy from './Katy.vue'
 import Contact from './Contact.vue'
+
+import Katy from './cities/Katy.vue'
+import Chicago from './cities/Chicago.vue'
+import Minneapolis from './cities/Minneapolis.vue'
+import Madison from './cities/Madison.vue'
+
+import Element from 'element-ui'
+// didn't download import 'element-ui/lib/theme-chalk/reset.css'
+import 'element-theme-chalk'
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
+Vue.use(Element, { size: 'small', zIndex: 3000 })
 
 //Set up Routes
 const routes = [
   { path: '/', component: Home },
   { path: '/katy', component: Katy },
-  { path: '/contact', component: Contact }
+  { path: '/contact', component: Contact },
+  { path: '/chicago', component: Chicago },
+  { path: '/minneapolis', component: Minneapolis },
+  { path: '/madison', component: Madison}
 ];
 
 const router = new VueRouter({

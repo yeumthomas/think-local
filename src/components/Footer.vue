66b5e256e406ffa215d14f16215a037d9d41df1e
Thomas Yeum <yeum.thomas@gmail.com>
2020-06-04 01:10:44 -0500

    Created footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="footer">
+    <p> Why did I build this website? Check out why <a href="/history-lesson">here</a>.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #c3e2f0;
+}
+
+#footer {
+    padding: 20px 0px;
+    max-width: 80%;
+    margin: auto;
+    
+}
+
+#footer a {
+    text-decoration: none;
+    color: #df7474;
+}
+</style>

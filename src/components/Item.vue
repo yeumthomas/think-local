b67679f3d3c5feac8960c48275e2db513493219b
Thomas Yeum <yeum.thomas@gmail.com>
2020-06-04 01:10:51 -0500

    Modified Items

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="item">
     <div class="city-list">
+      <h1 style="width: 100%; margin-left: auto; margin-right: auto;">Select a City to Support</h1>
       <div class='city' id ="chicago">
         <h1><a href='/chicago'> Chicago, IL</a></h1>
       </div>
@@ -9,6 +10,9 @@
       </div>
       <div class='city' id="madison">
         <h1><a href='/madison'> Madison, WI</a></h1>
+      </div>
+      <div class='city' id="philadelphia">
+        <h1><a href='/philadelphia'> Philadelphia, PA</a></h1>
       </div>
     </div>
   </div>
@@ -19,31 +23,31 @@
 export default {
   name: 'Item',
 
-  data () {
-    return {
-      chicpic: require('@/assets/chicago.jpeg'),
-      minnpic: require('@/assets/Minneapolis.jpeg'),
-      madpic: require('@/assets/madison.jpeg'),
+  // data () {
+  //   return {
+  //     chicpic: require('@/assets/chicago.jpeg'),
+  //     minnpic: require('@/assets/Minneapolis.jpeg'),
+  //     madpic: require('@/assets/madison.jpeg'),
 
-      cities: {
-        city1 : {
-          name: 'Chicago, IL',
-          link: 'chicago',
-          pic: '../chicago.jpeg'
-        },
-        city2: {
-          name: 'Minneapolis, MN',
-          link: 'minneapolis',
-          pic: '../Minneapolis.jpeg'
-        },
-        city3: {
-          name: 'Madison, WI',
-          link: 'madison',
-          pic: '../madison.jpeg'
-        }
-      }
-    }
-  }
+  //     cities: {
+  //       city1 : {
+  //         name: 'Chicago, IL',
+  //         link: 'chicago',
+  //         pic: '../chicago.jpeg'
+  //       },
+  //       city2: {
+  //         name: 'Minneapolis, MN',
+  //         link: 'minneapolis',
+  //         pic: '../Minneapolis.jpeg'
+  //       },
+  //       city3: {
+  //         name: 'Madison, WI',
+  //         link: 'madison',
+  //         pic: '../madison.jpeg'
+  //       }
+  //     }
+  //   }
+  // }
 
 }
 </script>
@@ -54,16 +58,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 220px;
-  display: flex;
+  color: white;
+  margin-top: 10vh;
+  background-color: #90b1d1;
 }
-
 
 .city-list {
   max-width: 700px;
   display: flex;
   flex-direction: column;
+  padding-bottom: 30px;
 }
 
 #chicago {
@@ -71,27 +75,20 @@ export default {
   background-size: cover;
 }
 
-/* #chicago:hover {
-  cursor: pointer;
-} */
-
 #minneapolis {
   background-image: url(../assets/Minneapolis.jpeg);
   background-size: cover;
 }
-
-/* #chicago:hover {
-  cursor: pointer;
-} */
 
 #madison {
   background-image: url(../assets/madison.jpeg);
   background-size: cover;
 }
 
-/* #madison:hover {
-  cursor: pointer;
-} */
+#philadelphia {
+  background-image: url(../assets/philly.jpg);
+  background-size: cover;
+}
 
 .city h1 a{
   color: white;
@@ -99,14 +96,17 @@ export default {
   display:inline-block;
   width: 100%;
   height: 100%;
+  border-radius: 10px 10px 10px 10px;
 }
+
 .city h1{
-  background-image: linear-gradient(transparent,rgba(0,0,0,.75));
+  background-image: linear-gradient(transparent,rgba(0,0,0,.45));
   min-height: 100%;
   width: 100%;
   text-align: left;
   padding: 10px 5px 10px 10px;
   margin: 0;
+  border-radius: 10px 10px 10px 10px;
 }
 
 .city {
@@ -119,37 +119,56 @@ export default {
   border-radius: 10px 10px 10px 10px;
 }
 
-/* 
-@media screen and (max-width: 499px) {
-  .city-list {
-    margin-top: 100px;
-    max-width: 350px;
+@media screen and (max-width: 399px) {
+  #item {
+    margin-top: 40vh;
   }
-  .city {
-    max-width: 92vw;
-    min-width: 92vw;
-    max-height: 150px;
-    min-height: 150px;
-    padding: 4px 4vw 4px 4vw;
-    margin: 0;
+  .city-list {
+    margin: 0px auto 0px auto;
+    max-width: 350px;
   }
   
-
-}
-
-@media screen and (min-width: 500px) {
-  .city-list {
-    margin-top: 100px;
-    max-width: 350px;
-
+  .city{
+    max-width: 300px;
+    min-width: 300px;
+    margin: 0px auto 15px auto;
   }
-
-  .city {
-  max-width: 150px;
-  min-width: 150px;
-  max-height: 100px;
-  min-height: 100px;
 }
 
-} */
+@media screen and (min-width: 400px) and (max-width: 500px) {
+  #item {
+    margin-top: 50vh;
+  }
+  .city-list {
+    margin: 0px auto 0px auto;
+    max-width: 350px;
+  }
+  
+  .city{
+    max-width: 350px;
+    min-width: 350px;
+    margin: 0px auto 15px auto;
+  }
+}
+
+@media screen and (min-width: 501px) and (max-width: 900px) {
+  #item {
+    padding-bottom: 3vh;
+    margin-top: 0;
+  }
+  .city-list {
+    margin: 0px auto 0px auto;
+    max-width: 80%;
+    min-width: 80%;
+  }
+  
+  .city{
+    max-width: 350px;
+    min-width: 350px;
+    margin: 0px auto 15px auto;
+  }
+}
+
+    /* flex-direction: row;
+    flex-wrap: wrap; */
 </style>
